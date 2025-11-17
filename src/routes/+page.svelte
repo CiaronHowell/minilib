df44d62de222347d6c6dc9267901fa9787c09e68
Ciaron Howell <ciaron.howell@stfc.ac.uk>
2025-11-17 22:08:29 +0000

    Using snippets

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import DataTable from './data-table.svelte';
-	import { columns } from './columns.js';
+	import { BookTable, columns } from '$lib/components/custom/book-table';
+	import AddBook from './add-book.svelte';
 
 	let { data } = $props();
 </script>
@@ -8,6 +8,10 @@
 <div class="flex grow justify-center">
 	<div class="flex w-2/3 flex-col p-5">
 		<h1 class="text-2xl font-bold">Your Library</h1>
-		<DataTable data={data.books} onlyOwner={true} {columns} />
+		<BookTable data={data.books} onlyOwner={true} {columns}>
+			{#snippet buttons()}
+				<AddBook />
+			{/snippet}
+		</BookTable>
 	</div>
 </div>
